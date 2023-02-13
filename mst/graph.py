c43149bb1f1ce3072a4df47bd2ac5bf86ac81c53
Bryson Choy <bryson.choy@ucsf.edu>
2023-02-13 11:02:56 -0800

    added comments

--- mst/graph.py
+++ mst/graph.py
@@ -53,8 +53,8 @@ class Graph:
         visited.append(0)
         h = []
 
-        # Start with the first row of the adjacency matrix, row_ind indicates the row number (starting node) of the adjacency matrix
-        # Remove all zeros from the row (no edges between those vertices)
+        # Start with the first row of the adjacency matrix
+        # Only add nonzero edges between nodes from the row (no edges between those vertices)
         
         # Push the first set of edge weights from vertex 0 to the priority queue
         row = self.adj_mat[0,:]
@@ -65,15 +65,13 @@ class Graph:
         while len(visited) < self.adj_mat.shape[0]:
 
             # Pop the lowest edge weight and find the destination node of that edge
-            # Although multiple identical MSTs may exist, we'll take the first destination node with that (unique) edge weight
             lowest = heapq.heappop(h)
             source = lowest[1]
             dest = lowest[2]
 
             # If the destination node has not been visited, add that lowest edge weight to the mst
-            # The row index becomes the destination node, and we visit that node
-            # The new row becomes the desintation node's row index of the adjacency matrix, push outgoing edges to the priority queue
-            # If the destination node has already been visited, then go back to the row containing the lowest edge weight from the priority queue
+            # The row index becomes the destination node, and we visit that destination node
+            # Push all outgoing edges, the source node (dest)  and the new destination nodes (ndset) to the priority queue
             if dest not in visited:
                 self.mst[source,dest] = lowest[0]
                 self.mst[dest,source] = lowest[0]
@@ -85,73 +83,4 @@ class Graph:
 
             if (not h) and (len(visited) < self.adj_mat.shape[0]): raise Exception("The input graph is disconnected")
 
-
-    """
-
-    def construct_mst(self):
-        
-    
-        TODO: Given `self.adj_mat`, the adjacency matrix of a connected undirected graph, implement Prim's 
-        algorithm to construct an adjacency matrix encoding the minimum spanning tree of `self.adj_mat`. 
-            
-        `self.adj_mat` is a 2D numpy array of floats. Note that because we assume our input graph is
-        undirected, `self.adj_mat` is symmetric. Row i and column j represents the edge weight between
-        vertex i and vertex j. An edge weight of zero indicates that no edge exists. 
-        
-        This function does not return anything. Instead, store the adjacency matrix representation
-        of the minimum spanning tree of `self.adj_mat` in `self.mst`. We highly encourage the
-        use of priority queues in your implementation. Refer to the heapq module, particularly the 
-        `heapify`, `heappop`, and `heappush` functions.
-
-        
-        #self.mst = None
-        self.mst = np.zeros(self.adj_mat.shape)
-
-        if self.adj_mat.size == 0: raise Exception("Input was an empty graph")
-        if np.any(self.adj_mat.diagonal()): raise Exception("There should be no self-edges in the graph")
-        if np.any(self.adj_mat < 0): raise Exception("All edge weights in the graph must be positive values")
-        #if np.any(self.adj_mat != self.adj_mat.T): raise Exception("The input graph must be a symmetric adjacency matrix")
-
-        # Initialize a list of visited nodes, priority queue of edge weights, and append vertex 0 to the visited list
-        visited = []
-        visited.append(0)
-        h = []
-
-        # Start with the first row of the adjacency matrix, row_ind indicates the row number (starting node) of the adjacency matrix
-        # Remove all zeros from the row (no edges between those vertices)
-        row = self.adj_mat[0,:]
-        row_ind = 0
-        insert = row[np.nonzero(row)]
-        
-        # Push the first set of edge weights from vertex 0 to the priority queue
-        for i in insert: 
-            heapq.heappush(h, i)
-        
-        # Loop until we have visited all nodes in the graph
-        while len(visited) < self.adj_mat.shape[0]:
-
-            # Pop the lowest edge weight and find the destination node of that edge
-            # Although multiple identical MSTs may exist, we'll take the first destination node with that (unique) edge weight
-            lowest = heapq.heappop(h)
-            dest = np.where(row == lowest)[0][0]
-
-            # If the destination node has not been visited, add that lowest edge weight to the mst
-            # The row index becomes the destination node, and we visit that node
-            # The new row becomes the desintation node's row index of the adjacency matrix, push outgoing edges to the priority queue
-            # If the desintation node has already been visited, then go back to the row containing the lowest edge weight from the priority queue
-            if dest not in visited:
-                self.mst[row_ind,dest] = lowest
-                row_ind = dest
-                visited.append(dest)
-                
-                row = self.adj_mat[dest,:]
-                insert = row[np.nonzero(row)]
-                for i in insert: 
-                    heapq.heappush(h, i)
-            else: 
-                row = self.adj_mat[np.where(self.adj_mat == lowest)[0][0]]
-                row_ind = np.where(self.adj_mat == lowest)[0][0]
-
-            if (not h) and (len(visited) < self.adj_mat.shape[0]): raise Exception("The input graph is disconnected")
-    """
 
